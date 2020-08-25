aa8e004e882c793e977584a4cad47168d9ff3926
Hunor Kovacs <hkovacs@hiya.com>
2020-08-26 00:25:21 +0200

    Wrapped akka system in IO. Crazy

--- itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
+++ itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
@@ -3,31 +3,58 @@ package com.hunorkovacs.itsaren.simple
 import akka.Done
 import akka.actor.{ActorSystem, CoordinatedShutdown}
 import akka.http.scaladsl.Http
-import akka.stream.Materializer
+import cats.effect.{ContextShift, ExitCode, IO, IOApp, Resource}
 import com.hunorkovacs.itsaren.simple.crib.InMemCribDbService
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent.Future
 
-object ItsAren extends App {
+object ItsAren extends IOApp {
 
-  private val logger                     = LoggerFactory.getLogger(getClass)
-  implicit private val sys: ActorSystem  = ActorSystem("itsmycrib")
-  implicit private val mat: Materializer = Materializer(sys)
-  import sys.dispatcher
+  implicit val contextShift2: ContextShift[IO] = contextShift
 
-  private val cribDbService = new InMemCribDbService()
-  private val router        = new Router(cribDbService)
-
-  private val httpBindingF = Http().newServerAt("0.0.0.0", 8080).bindFlow(router.route)
-  logger.info("Server online, accessible on port=8080")
-  logger.info("Press Ctrl-C (or send SIGINT) to stop.")
-
-  CoordinatedShutdown(sys).addCancellableTask(CoordinatedShutdown.PhaseBeforeServiceUnbind, "log-shutdown") { () =>
-    Future {
-      logger.info("Exiting...")
-      Done
-    }
+  override def run(args: List[String]): IO[ExitCode] = {
+    val k = for {
+      logger    <- Resource.liftF(IO(LoggerFactory.getLogger(getClass)))
+      sys       <- createActorSystemResource(logger)
+      boundHttp <- Resource.liftF(bindHttp(sys, logger))
+    } yield boundHttp
+    k.use(_ => IO.never).as(ExitCode.Success)
   }
 
+  private def createActorSystemResource(logger: Logger): Resource[IO, ActorSystem] =
+    Resource.make {
+      IO {
+        val sys = ActorSystem("itsaren")
+        setupCoordinatedShutdown(sys, logger)
+        sys
+      }
+    } { sys =>
+      IO.fromFuture(IO(sys.terminate())).as(() => ())
+    }
+
+  private def setupCoordinatedShutdown(sys: ActorSystem, logger: Logger) =
+    CoordinatedShutdown(sys).addCancellableTask(CoordinatedShutdown.PhaseBeforeServiceUnbind, "log-shutdown") { () =>
+      Future {
+        logger.info("Exiting...")
+        Done
+      }(sys.dispatcher)
+    }
+
+  private def bindHttp(sys: ActorSystem, logger: Logger): IO[Http.ServerBinding] =
+    for {
+      httpBinding <- IO.fromFuture {
+                       implicit val imSys: ActorSystem = sys
+                       val cribDbService               = new InMemCribDbService()
+                       val router                      = new Router(cribDbService)
+                       val httpBinding                 = Http(sys).newServerAt("0.0.0.0", 8080).bindFlow(router.route)
+                       IO(httpBinding)
+                     }
+      _           <- IO(
+                       logger.info(
+                         "Server online, accessible on port=8080 " +
+                           "Press Ctrl-C (or send SIGINT) to stop"
+                       )
+                     )
+    } yield httpBinding
 }
