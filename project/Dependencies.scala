53efe703535b37cf92fdaad2890bcc37b9ad7773
Hunor Kovacs <hkovacs@hiya.com>
2020-08-26 21:24:40 +0200

    Zio start

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,22 @@ import sbt._
 
 object Dependencies {
 
-  private val CatsVersion     = "2.1.4"
-  private val Http4sVersion   = "0.21.7"
-  private val CirceVersion    = "0.12.3"
+  private val CatsVersion   = "2.1.4"
+  private val Http4sVersion = "0.21.7"
+  private val CirceVersion  = "0.12.3"
 
   val ItsarenSimple = Seq(
-    "org.typelevel"     %% "cats-effect"           % CatsVersion,
-    "io.circe"          %% "circe-core"            % CirceVersion,
-    "io.circe"          %% "circe-generic"         % CirceVersion,
-    "io.circe"          %% "circe-parser"          % CirceVersion,
-    "org.http4s"        %% "http4s-dsl"            % Http4sVersion,
-    "org.http4s"        %% "http4s-blaze-server"   % Http4sVersion,
-    "org.http4s"        %% "http4s-circe"          % Http4sVersion,
-    "org.slf4j"          % "slf4j-api"             % "1.7.30",
-    "ch.qos.logback"     % "logback-classic"       % "1.2.3"
+    "org.typelevel" %% "cats-effect"         % CatsVersion,
+    "dev.zio"       %% "zio"                 % "1.0.1",
+    "dev.zio"       %% "zio-interop-cats"    % "2.1.4.0",
+    "io.circe"      %% "circe-core"          % CirceVersion,
+    "io.circe"      %% "circe-generic"       % CirceVersion,
+    "io.circe"      %% "circe-parser"        % CirceVersion,
+    "org.http4s"    %% "http4s-dsl"          % Http4sVersion,
+    "org.http4s"    %% "http4s-blaze-server" % Http4sVersion,
+    "org.http4s"    %% "http4s-circe"        % Http4sVersion,
+    "org.slf4j"      % "slf4j-api"           % "1.7.30",
+    "ch.qos.logback" % "logback-classic"     % "1.2.3"
   )
 
 }
