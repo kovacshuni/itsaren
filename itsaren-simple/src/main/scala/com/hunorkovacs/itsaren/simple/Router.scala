cf5515ef044e423fa8d37e47e77984242a515d8f
Hunor Kovacs <kovacshuni@gmail.com>
2020-10-01 18:08:09 +0200

    Remove unnecessary parts for now

--- itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/Router.scala
+++ itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/Router.scala
@@ -4,17 +4,6 @@ import cats.data.Kleisli
 import zio.Task
 import org.http4s._
 
-// import cats.data.Kleisli
-// import cats.effect.IO
-// import com.hunorkovacs.itsaren.simple.crib.{CribDbService, CribNoId}
-// import com.hunorkovacs.itsaren.simple.message.Message
-// import io.circe.generic.auto._
-// import org.http4s.circe.CirceEntityEncoder._
-// import org.http4s.circe._
-// import org.http4s.dsl.io._
-// import org.http4s.implicits._
-// import org.http4s.{EntityDecoder, HttpRoutes, Request, Response}
-
 object Router {
 
   trait Service {
@@ -23,46 +12,6 @@ object Router {
 
   }
 
-
-
-  // def http4sRoutes(cribDbService: CribDbService): Kleisli[IO, Request[IO], Response[IO]] =
-  //   HttpRoutes
-  //     .of[IO] {
-  //       case GET -> Root / "cribs"            =>
-  //         cribDbService.retrieveAll.flatMap(Ok(_))
-
-  //       case GET -> Root / "cribs" / id       =>
-  //         cribDbService.retrieve(id).flatMap {
-  //           case None       => NotFound()
-  //           case Some(crib) => Ok(crib)
-  //         }
-
-  //       case req @ POST -> Root / "cribs"     =>
-  //         for {
-  //           cribNoId <- req.as[CribNoId]
-  //           crib     <- cribDbService.create(cribNoId)
-  //           resp     <- Ok(crib)
-  //         } yield resp
-
-  //       case req @ PUT -> Root / "cribs" / id =>
-  //         for {
-  //           cribNoId <- req.as[CribNoId]
-  //           crib     <- cribDbService.update(id, cribNoId)
-  //           resp     <- crib match {
-  //                         case Some(updatedCrib) => Ok(updatedCrib)
-  //                         case None              => NotFound(Message("Crib not found, can't update."))
-  //                       }
-  //         } yield resp
-
-  //       case DELETE -> Root / "cribs" / id    =>
-  //         cribDbService.delete(id).flatMap {
-  //           case None    => NotFound(Message("Crib not found, can't delete."))
-  //           case Some(_) => NoContent()
-  //         }
-  //     }
-  //     .orNotFound
-
-  // implicit val cribNoIdDecoder: EntityDecoder[IO, CribNoId] = jsonOf[IO, CribNoId]
 }
 
 trait Router {
