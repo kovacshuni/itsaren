a0d25691638a1c38310e2277f0e4a0a9f5a691b1
Hunor Kovacs <kovacshuni@gmail.com>
2020-10-01 18:06:42 +0200

    without Console dependency, but my dependency is passed, is ok. step by step.

--- itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
+++ itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
@@ -1,47 +1,34 @@
 package com.hunorkovacs.itsaren.simple
 
 import zio._
-import zio.console._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 
-import org.slf4j.LoggerFactory
-import org.slf4j.Logger
-
 object ItsAren extends App {
 
-  type RouterWithConsole = RouterLive with Console
-
-  // private def f(c: Console): Logger = LoggerFactory.getLogger(ItsAren.getClass)
-
-  def run(args: List[String]): zio.URIO[zio.ZEnv, ExitCode] =
+  def run(args: List[String]): zio.URIO[zio.ZEnv, ExitCode] = {
     ZIO
       .runtime[ZEnv]
       .flatMap { implicit runtime =>
-        val resources = for {
+        val serverResource = for {
           helloWorldService <- ZManaged.fromEffect(ZIO.access[Router](_.router.helloWorldService))
           server            <- BlazeServerBuilder[Task](runtime.platform.executor.asEC)
                                  .bindHttp(8080, "localhost")
                                  .withHttpApp(helloWorldService)
                                  .resource
                                  .toManagedZIO
-          logger            <- ZManaged.succeed(LoggerFactory.getLogger(ItsAren.getClass))
-        } yield (server, logger)
+        } yield server
 
-        val using = resources
-          .use { r =>
-            r match {
-              case (_, logger) =>
-                ZIO(logger.info("Server online, accessible on port=8080 Press Ctrl-C (or send SIGINT) to stop"))
-                  .flatMap(_ => ZIO.never)
-            }
+        val used = serverResource
+          .use { _ =>
+            ZIO(println("Server online, accessible on port=8080 Press Ctrl-C (or send SIGINT) to stop"))
+              .flatMap(_ => ZIO.never)
           }
           .as(ExitCode.success)
-          .catchAllCause(err => putStrLn(err.prettyPrint).as(ExitCode.failure))
+          .catchAll(_ => ZIO.succeed(ExitCode.failure))
 
-        using
-          .provide(Console. live.++(RouterLiveObj.apply))
+        used.provide(RouterLiveObj)
       }
-
+  }
 }
