2aba633af554c430abda1fb97abb9bd27b73ce8e
Hunor Kovacs <kovacshuni@gmail.com>
2020-10-02 17:27:51 +0200

    Combining more layers

--- itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
+++ itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/ItsAren.scala
@@ -2,20 +2,29 @@ package com.hunorkovacs.itsaren.simple
 
 import zio._
 import zio.console.Console
+import zio.random.Random
+import zio.clock.Clock
 import com.hunorkovacs.itsaren.simple.UserRepo.DBError
 
 object ItsAren extends App {
 
   def run(args: List[String]): URIO[ZEnv, ExitCode] = {
-    val user2: User = User(UserId(123), "Tommy")
-    val makeUser: ZIO[Logging with UserRepo, DBError, Unit] = for {
-      _ <- Logging.info(s"hi, inserting user")
-      _ <- UserRepo.createUser(user2)
-      _ <- Logging.info(s"user inserted, bye")
+    val makeUser: ZIO[Logging with UserRepo with Random with Clock, DBError, Unit] = for {
+      userId    <- zio.random.nextLong.map(UserId.apply)
+      createdAt <- zio.clock.currentDateTime.orDie
+      user       = User(userId, "Tommy")
+      _         <- Logging.info(s"hi, inserting user")
+      _         <- UserRepo.createUser(user)
+      _         <- Logging.info(s"user=$user inserted, bye")
     } yield ()
 
-    val horizontal: ZLayer[Console, Nothing, Logging with UserRepo] = Logging.consoleLogger ++ UserRepo.inMemoryRepo
-    val fullLayer: Layer[Nothing, Logging with UserRepo] = Console.live >>> horizontal
+    val horizontal: ZLayer[Console, Nothing, Logging with UserRepo with Random with Clock] =
+      Logging.consoleLogger ++
+        UserRepo.inMemoryRepo ++
+        Random.live ++
+        Clock.live
+
+    val fullLayer: Layer[Nothing, Logging with UserRepo with Random with Clock] = Console.live >>> horizontal
 
     val provided = makeUser.provideLayer(fullLayer)
 
--- itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/User.scala
+++ itsaren-simple/src/main/scala/com/hunorkovacs/itsaren/simple/User.scala
@@ -2,4 +2,4 @@ package com.hunorkovacs.itsaren.simple
 
 case class User(id: UserId, name: String)
 
-case class UserId(id: Int)
+case class UserId(id: Long)
